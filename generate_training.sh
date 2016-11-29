028e8a31ad3a004ddfc24d9c65d84bb7b807d93f
Maxim Shayduk <mshayduk@gmail.com>
2016-11-29 17:04:33 +0100

    temp update

--- generate_training.sh
+++ generate_training.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 if [ "$1" == "-h" ]; then
   echo "Makes ground truth from png pages and corresponding txt files"
-  echo "Usage: `basename $0` [pages directory]"
+  echo "Usage: `basename $0` [pdffile_pages directory (write with slash at the end: /)]"
   exit 0
 fi
 
 mkdir -p $1'/book/'
-#ls $1/*.png
 
+prefix=`echo $1 | sed -e "s/\_pages\/$//" | awk -F '/' '{print $NF}'`
+echo $prefix
 # binarize pages
 #ocropus-nlbin -n $1/test-????.png -o $1'/book/'
 
@@ -20,16 +21,18 @@ cnt=0
 for b in $(ls -p $1/'book/' | grep '/');
 	do
 		txt=$(ls $1/*$cnt.txt | head -1)
-		cp $txt $1/book/$b/ 
-		#echo $1/book/$b $cnt $npages $txt
+		#cp $txt $1/book/$b/ 
+		
+		echo $1/book/$b $cnt $npages $txt
 		#splitting txt to gt.txt lines
 		#for l in $1/book/$b/*.png;
+		
 		linecnt=0
 		for l in $(ls -1 $1/book/$b/*bin.png | sed -e 's/\.bin.png$//'); #only names, no extentions
 			do
 				linecnt=$(($linecnt+1))
-				#echo $l $linecnt $(sed -n "${linecnt}{p;q;}" $txt)
-				$(sed -n "${linecnt}{p;q;}" $txt > $l.gt.txt) 
+				echo $l $linecnt $(sed -n "${linecnt}{p;q;}" $txt)
+				# $(sed -n "${linecnt}{p;q;}" $txt > $l.gt.txt) 
 			done;
 
 		cnt=$(($cnt+1))

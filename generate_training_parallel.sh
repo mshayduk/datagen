0f8f7ff032cb72c9db7b223ee9af464fb9b4d158
Maxim Shayduk <mshayduk@gmail.com>
2016-12-02 16:45:32 +0100

    run with gnu parallel

---
+++ generate_training_parallel.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+if [ "$1" == "-h" ]; then
+  echo "Makes ground truth from png pages and corresponding txt files"
+  #echo "Usage: `basename $0` [pdffile_pages directory (write with slash at the end: /)]"
+  echo " PARALLEL: run with command : ls -d ./GDATA/*pages/ | parallel --no-notice -j3 ./generate_training_parallel.sh {}"
+  exit 0
+fi
+
+#useful trick: passes result of find to identify
+# find ./0246/*.png -print0 | xargs -0 identify
+
+mkdir -p $1'/book/'
+
+prefix=`echo $1 | sed -e "s/\_pages\/$//" | awk -F '/' '{print $NF}'`
+echo $prefix
+
+# binarize pages (no skew is assumed, no fancy threshold estimation)
+echo 'binarization in $1 '
+#ocropus-nlbin -n --maxskew 0 --escale 0 --lo 0 --hi 1 --parallel 6 $1/$prefix-????.png -o $1'/book/'
+#exit
+
+# segment pages (only one column assumed, no noise removal )
+echo 'segmentation in $1'
+ocropus-gpageseg -n --maxseps 0 --maxcolseps 0 --minscale 10 --noise 0 --parallel 5 $1'/book/????.bin.png'
+exit
+#ocropus-gpageseg -n --maxseps 0 --maxcolseps 0 --csminheight 90 --minscale 10 --noise 0 --parallel 6 './GDATA/qcr_tiny_regular_pages/book/????.bin.png'
+
+# make ground truth text
+# NOTE: segmented pages pagenum (in book/[pagenum]/) starts from 1, txt pages start from 0 !
+cnt=0
+for b in $(ls -p $1/'book/' | grep '/');
+	do
+		# get correct id, if file sequence has gaps due to the failed previous steps
+		id=`ls -d $1/book/$b | awk -F '/' '{print $(NF-1)}' | sed -e 's:^0*::'`
+		id=`echo $(($id - 1))`
+		txt=$(ls $1/*$id.txt | head -1)
+		#txt=$(ls $1/*$cnt.txt | head -1)
+		#cp $txt $1/book/$b/ 
+
+		#echo 'making gt: ' $1/book/$b  'id='$id 'cnt='$cnt $txt
+		#splitting txt to gt.txt lines
+		#for l in $1/book/$b/*.png;
+		
+		#skip pages with number of segments not equal to the number of lines
+		nsnippets=$(ls -1 $1/book/$b/*bin.png | wc -l)
+		nlines=$(cat $txt | wc -l)	
+		if [ $nsnippets != $nlines ]; then
+			continue
+		fi
+
+		linecnt=0
+		for l in $(ls -1 $1/book/$b/*bin.png | sed -e 's/\.bin.png$//'); #only names, no extentions
+			do
+				linecnt=$(($linecnt+1))
+				echo $l $linecnt $nsnippets $nlines $(sed -n "${linecnt}{p;q;}" $txt)
+				$(sed -n "${linecnt}{p;q;}" $txt > $l.gt.txt) 
+			done;
+
+		cnt=$(($cnt+1))
+	done;
---
+++ pdf2textpages_parallel.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+if [ "$1" == "-h" ]; then
+  echo "Converts multipage pdf to text and png files (one txt and png file per page)"
+  echo "Usage: `basename $0` [filename.pdf]"
+  exit 0
+fi
+datagensys="/home/maxim/Projects/Doker/sandbox/traindatagen/python/"
+datadir=$(dirname "$1")
+filename=$(basename "$1")
+extension="${filename##*.}"
+name="${filename%.*}"
+#name=$(echo $1 | cut -f 1 -d '.')
+npages=$(pdfinfo $1 | grep Pages | sed 's/[^0-9]*//')
+droplast=50
+npages=$(($npages-$droplast-1))
+
+mkdir -p $datadir'/'$name'_pages'
+#find $name'_pages/*' -print0 | xargs -0 rm -rv
+cp $1 $datadir'/'$name'_pages'
+cd $datadir'/'$name'_pages'
+
+# coverting in parallel:
+# find . -name '*-000*.pdf' | parallel --no-notice -j 4 convert -density 300 {} {}.png
+
+for f in $1;
+	do
+    	page=0
+    	for i in $(seq -f "%04g" 0 $npages); 
+        	do
+        	page=$(($page+1))
+        	pdftotext -f "$page" -l $l "$page" -layout $name'.pdf' "${name%.pdf}-$i.txt";
+        	pdftk $name'.pdf' cat $page'-'$page output "${name%.pdf}-$i.pdf"
+        	#convert -density 300 "${name%.pdf}-$i.pdf" "${name%.pdf}-$i.png" 
+    		echo $f "${name%.pdf}-$i.txt" $i "$page"
+    		python $datagensys'/utf8normalize.py' "NFC" "${name%.pdf}-$i.txt"
+    	done;
+
+    	find . -name '*-*.pdf' | parallel --no-notice -j7 convert -density 300 {} {}.png
+    	rename 's/\.pdf\.png/\.png/g' *.pdf.png
+    	#find . -name '*-*.pdf' | parallel --no-notice -j4 echo {}
+done
+
+#convert -density 300 $name.pdf $name-%04d.png 
